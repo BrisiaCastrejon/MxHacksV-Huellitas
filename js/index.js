da3c2fd071cf9ebd8a1e995545eddbd573aa6140
alppacat <lorena.pratts@gmail.com>
2018-09-08 10:49:55 -0500

    Arregla referencias entre paginas

--- js/index.js
+++ js/index.js
@@ -5,11 +5,11 @@ const btnDenunciar = document.getElementById('denunciar');
 btnDenunciar.addEventListener('click', event => {
   location.href = '../denuncias.html';
 });  
-// btnGoogle.addEventListener('click', event => {
-//   let provider = new firebase.auth.GoogleAuthProvider();
-//   const promise = firebase.auth().signInWithRedirect(provider);
-//   promise.catch(event => alert(event.message));
-// });
+btnGoogle.addEventListener('click', event => {
+  let provider = new firebase.auth.GoogleAuthProvider();
+  const promise = firebase.auth().signInWithRedirect(provider);
+  promise.catch(event => alert(event.message));
+});
 btnFacebook.addEventListener('click', event => {
   let provider = new firebase.auth.FacebookAuthProvider();
   firebase
@@ -27,18 +27,9 @@ btnFacebook.addEventListener('click', event => {
       console.log(errorCode);
     });
 });
-// btnFacebook.addEventListener('click', event => {
-//   let provider = new firebase.auth.FacebookAuthProvider();
-//   firebase.auth().signInWithRedirect(provider)
-//     .then((result)=> {
-//       let token = result.credencia.accessToken;
-//       let user = result.user;
-//     })
-//     .catch((error) => {
-//       let errorCode = error.code;
-//       let errorMessage = error.message;
-//       let email = error.email;
-//       let credential = error.credential;
-//       console.log(errorCode);
-//     });
-// });
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if (firebaseUser) {
+    window.location.assign('views/index.html');
+  };
+});
