bdecef8163d4b4edad89ce030d09f738145bd29b
alppacat <lorena.pratts@gmail.com>
2018-09-08 17:39:30 -0500

    Guarda foto en base de datos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
+
+const btnLogout = document.getElementById('btn-logout');
 let database = firebase.database();
+const boxBtn = document.getElementById('logout-box');
+
+const createUser = (name, email, photo) => {
+  let conect = userAuth.push({
+    name: name,
+    email: email,
+    photo: photo
+  });
+};
+
+btnLogout.addEventListener('click', event => {
+  firebase.auth().signOut();
+  btnLogout.style.display = 'none';
+  window.location.assign('../login.html');
+
+});
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
   let user = firebase.auth().currentUser;
@@ -8,17 +26,12 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
     });
     if (firebaseUser) {
       const userName = document.getElementById('user-name');
+      const userPhoto = document.getElementById('user-photo');
       userName.innerHTML = user.displayName;
+      userPhoto.innerHTML = `<img id="picture-profile" src="${user.photoURL}>`;
     }
   }
   let id = user.uid;
   userAuth = database.ref('users/' + id);
-  createUser(user.displayName, user.email);
+  createUser(user.displayName, user.email, user.photoURL);
 });
-
-createUser = (name, email) => {
-  let conect = userAuth.push({
-    name: name,
-    email: email
-  });
-};
